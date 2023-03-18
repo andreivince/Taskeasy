71d9c467ac30f4d469dceb40fad4fe25f4b6f4fe
andreivince <andreivince21@gmail.com>
2023-03-18 15:09:01 -0400

    I need to adjust

--- task-everyday/script.js
+++ task-everyday/script.js
@@ -1,98 +1,153 @@
-function display() {
-    // Get the input element and its value
-    const input = document.querySelector('input');
-    const value = input.value;
-
-    // Create a new list item element and a checkbox element
-    const li = document.createElement('li');
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    const delete_button = document.createElement('input')
-    delete_button.type = 'button';
-    delete_button.value = 'X';
-
-    // Add the checkbox and text to the list item
-    li.appendChild(checkbox);
-    li.appendChild(document.createTextNode(value));
-    li.appendChild(delete_button)
-    
-    
-
-    // Add the new list item to the existing list
-    const ul = document.getElementById('output');
-    ul.appendChild(li);
-
-    // Clear the input field
-    input.value = '';
-
-    // Listen for change event on checkbox and remove li if checked
-    delete_button.addEventListener('click', function() {
-        const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-        const index = tasks.indexOf(li.textContent);
-        if (index > -1) {
-          tasks.splice(index, 1);
-        }
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-        li.remove();
-      });
+const now = new Date()
+const tomorrow = new Date(+1)
+const untilTomorrow = tomorrow - now
 
 
-    checkbox.addEventListener('change', function() {
-        if (checkbox.checked) {
-            li.remove();
-          }
-        });
+// Get references to the input and output elements
+const newTaskInput = document.getElementById('new_task');
+const taskListOutput = document.getElementById('output');
 
-    // Store task in local storage and retrieve tasks from local storage
-    if (value) {
-        const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-        tasks.push(value);
-        localStorage.setItem('tasks', JSON.stringify(tasks));
+// Load the tasks from localStorage on page load
+window.addEventListener('load', loadTasks);
+
+// Add event listener to the "Enter" button
+document.getElementById('enter-button').addEventListener('click', addTask);
+
+// Function to load tasks from localStorage
+// Function to load tasks from localStorage
+function loadTasks() {
+    // Get the tasks from localStorage as a JSON string
+    const tasksJSON = localStorage.getItem('tasks');
+  
+    // If tasks exist in localStorage, parse them and add them to the task list
+    if (tasksJSON !== null) {
+      const tasks = JSON.parse(tasksJSON);
+  
+      for (const task of tasks) {
+        addTaskToList(task.taskText);
+      }
     }
-}
-
-// This function is called every 5000 milliseconds. It retrieves tasks from local storage
-// and adds them to the list if they are not already present. If the checkbox is checked or
-// the delete button is clicked, the task is removed from the list and from local storage, respectively.
-function everyday() {
-    const ul = document.getElementById('output');
-    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    tasks.forEach(function(task) {
-        // Check if task is already in the list
-        const taskExists = [...ul.children].some(function(li) {
-            return li.textContent === task;
-        });
-        if (!taskExists) {
-            const li = document.createElement('li');
-            const checkbox = document.createElement('input');
-            checkbox.type = 'checkbox';
-            const delete_button = document.createElement('input')
-            delete_button.type = 'button';
-            delete_button.value = 'X';
-            li.appendChild(checkbox);
-            li.appendChild(document.createTextNode(task));
-            li.appendChild(delete_button);
-            ul.appendChild(li);
-            delete_button.addEventListener('click', function() {
-                const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-                const index = tasks.indexOf(li.textContent);
-                if (index > -1) {
-                  tasks.splice(index, 1);
-                }
-                localStorage.setItem('tasks', JSON.stringify(tasks));
-                li.remove();
-              });
-
-            checkbox.addEventListener('change', function() {
-                if (checkbox.checked) {
-                    li.remove();
-                }
-            });
-        }
+  }
+  
+  // Function to add a new task to the task list
+  function addTaskToList(taskText) {
+    // Create a new list item element
+    const newTaskListItem = document.createElement('li');
+  
+    // Set the text of the list item to the task text
+    newTaskListItem.innerText = taskText;
+  
+    // Create "delete" and "hide" buttons for the task
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.addEventListener('click', () => {
+      // Remove the task from the task list and local storage
+      newTaskListItem.remove();
+      removeTaskFromLocalStorage(taskText);
     });
+  
+    const hideButton = document.createElement('button');
+    hideButton.innerText = 'Hide';
+    hideButton.addEventListener('click', () => {
+      // Hide the task for 5 seconds
+      newTaskListItem.style.display = 'none';
+      setTimeout(() => {
+        newTaskListItem.style.display = 'block';
+      }, 500);
+    });
+  
+    // Add the buttons to the task list item
+    newTaskListItem.appendChild(deleteButton);
+    newTaskListItem.appendChild(hideButton);
+  
+    // Add the list item to the task list output
+    taskListOutput.appendChild(newTaskListItem);
+  }
+  
+// Function to add a new task to the list
+function addTask() {
+  // Get the value of the input field
+  const taskText = newTaskInput.value;
+
+  // Add the new task to the local storage
+  addTaskToLocalStorage(taskText);
+
+  // Add the new task to the task list
+  addTaskToList(taskText);
+
+  // Clear the input field
+  newTaskInput.value = '';
 }
 
-const now = new Date();
-const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
-const timeUntilTomorrow = tomorrow - now;
-setInterval(everyday, timeUntilTomorrow);
+// Function to add a new task to the local storage
+function addTaskToLocalStorage(taskText) {
+  // Get the existing tasks from localStorage as a JSON string
+  const tasksJSON = localStorage.getItem('tasks');
+
+  // If no tasks exist in localStorage, create an empty array
+  let tasks = [];
+
+  if (tasksJSON !== null) {
+    tasks = JSON.parse(tasksJSON);
+  }
+
+  // Add the new task to the tasks array
+  tasks.push(taskText);
+
+  // Save the updated tasks to localStorage as a JSON string
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Function to add a new task to the task list
+function addTaskToList(taskText) {
+  // Create a new list item element
+  const newTaskListItem = document.createElement('li');
+
+  // Set the text of the list item to the task text
+  newTaskListItem.innerText = taskText;
+
+  // Create "delete" and "hide" buttons for the task
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = 'Delete';
+  deleteButton.addEventListener('click', () => {
+    // Remove the task from the task list and local storage
+    newTaskListItem.remove();
+    removeTaskFromLocalStorage(taskText);
+  });
+
+  const hideButton = document.createElement('button');
+  hideButton.innerText = 'Hide';
+  hideButton.addEventListener('click', () => {
+    // Hide the task for 5 seconds
+    newTaskListItem.style.display = 'none';
+    setTimeout(() => {
+      newTaskListItem.style.display = 'block';
+    }, 500);
+  });
+
+  // Add the buttons to the task list item
+  newTaskListItem.appendChild(deleteButton);
+  newTaskListItem.appendChild(hideButton);
+
+  // Add the list item to the task list output
+  taskListOutput.appendChild(newTaskListItem);
+}
+
+// Function to remove a task from the local storage
+function removeTaskFromLocalStorage(taskText) {
+  // Get the existing tasks from localStorage as a JSON string
+  const tasksJSON = localStorage.getItem('tasks');
+
+  // If no tasks exist in localStorage, return
+  if (tasksJSON === null) {
+    return;
+  }
+
+  // Parse the tasks JSON string into an array
+  const tasks = JSON.parse(tasksJSON);
+
+  // Find the index of the task to remove
+  const taskIndex = tasks.indexOf(taskText);
+
+}
+// If the task is not found, return
